d03913babdf4c10ff80ee11977af57cbeaf86d2c
enes <eymurtepeenes@gmail.com>
2021-05-30 13:47:55 +0300

    JSWork1 is added

--- 04-javascript-kategorisi/js/015-kosullarla-calismak.js
+++ 04-javascript-kategorisi/js/015-kosullarla-calismak.js
@@ -6,7 +6,7 @@ let username = prompt("Kullanici Adinizi Giriniz:")
 // eger (username.length > 0) {console.log(username)} degilse {console.log("bilgi yok")}
 // if (username.length > 0) {console.log(username)} else {console.log("bilgi yok")}
 
-if (username) {  // if kismi her zaman true ise calisir
+if (username) { // if kismi her zaman true ise calisir
     console.log(`Kullanici Bilginiz ${username}`)
 } else {
     console.log("bilgi yok")
---
+++ JavaScript/css-class-eklemek-veya-cikarmak/Demo/demo.js
@@ -0,0 +1,30 @@
+function changeColor() {
+    document.getElementById("demo").style.background = "red";
+
+}
+
+let change = document.querySelector('#change')
+change.classList.add("text-primary")
+change.classList.add("font-size-24")
+change.style.background = "orange"
+
+change.classList.remove("font-size-24")
+
+change.classList.replace("text-primary", "font-italic")
+
+
+console.log(change.classList.item([0]));
+
+
+
+
+let yesOrNot = document.querySelector('.yesOrNot')
+yesOrNot.classList.add("text-primary");
+
+
+$(document).ready(function () {
+    $("span").click(function () {
+        alert($("span").hasClass("text-primary"));
+    });
+});
+
